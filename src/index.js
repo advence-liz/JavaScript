78395b808f519b72e53b54683f3aff406a605bb2
advence-liz <1012515183@qq.com>
2017-12-12 09:58:16 +0800

    更新格式

--- src/index.js
+++ src/index.js
@@ -14,14 +14,16 @@ class NewsSubscriber extends BaseSubscriber {
 
 var publisher = new BasePublisher();
 var subscribe = new NewsSubscriber( function () { console.log(this.liz); }, { liz: 'liz' });
-subscribe.toString();
 var subscribe1 = new NewsSubscriber( function () { console.log(this.liz); }, { liz: 'liz1' });
+
 var handle= publisher.subscribe(subscribe);
 publisher.subscribe(subscribe1);
 console.log('通过 handle 触发单个 subscribe');
 publisher.publish(handle);
+
 console.log("通过 topic 触发一组 subscribe");
 publisher.publish('news');
-console.log('同 handle unsubscribe 指定 subscribe 然后触发一组');
+
+console.log('通过 handle unsubscribe 指定 subscribe 然后触发一组');
 publisher.unsubscribe(handle);
 publisher.publish('news');
