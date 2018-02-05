211a368bd92f21a490b0b1b3c52d87fce421d2a1
advence-liz <1012515183@qq.com>
2018-02-05 17:31:55 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 
 import DateFormat from "./DateFormat";
 
-var liz =new DateFormat().format("yYyyMMddhhmmss");
-console.log(new DateFormat().format("yYyyMMDdhhmmss"));
-console.log(new DateFormat().format("yYyyMMDDHhmmss"));
-console.log(new DateFormat().format("yYyyMMddhhmmss"));
-console.log(new DateFormat().format("yyyyMMddhhmmss"));
+console.group('Date Format');
+console.log('yYyyMMDdhhmmss',new DateFormat().format("yYyyMMDdhhmmss"));
+console.log('yYyyMMDDHhmmss',new DateFormat().format("yYyyMMDDHhmmss"));
+console.log('yYyyMMddhhmmss',new DateFormat().format("yYyyMMddhhmmss"));
+console.log('yyyyMMdDHhmmss',new DateFormat().format("yyyyMMdDHhmmss"));
+console.groupEnd('Date Format');
