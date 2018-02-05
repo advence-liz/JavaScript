6981ac87883abb62295f7974f6c8d38d7ef8103f
advence-liz <1012515183@qq.com>
2018-02-05 17:20:45 +0800

    y q d h 年 季度 日 小时 不区分大小写

--- src/DateFormat.jsx
+++ src/DateFormat.jsx
@@ -27,6 +27,10 @@ class DateFormat {
     isValidDate() {
         return !isNaN(this.date.getDay());
     }
+    /**
+     *  y q d h 年 季度 日 小时 不区分大小写
+     * @param {String} fmt 
+     */
     format(fmt) {
         var o = {
             "M+": this.getMonth() + 1, //月份 
@@ -37,9 +41,16 @@ class DateFormat {
             "q+": Math.floor((this.getMonth() + 3) / 3), //季度 
             "S": this.getMilliseconds() //毫秒 
         };
-        if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
-        for (var k in o)
-            if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+        if (/(y+)/i.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+        for (var k in o) {
+            var reg = k === 'h+' || k === 'd+' ||k ==='q+'?
+                reg = new RegExp("(" + k + ")", 'i') :
+                reg = new RegExp("(" + k + ")");
+
+            if (reg.test(fmt)) {
+                fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+            }
+        }
         return fmt;
     }
 
--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 
+import DateFormat from "./DateFormat";
+
+var liz =new DateFormat().format("yYyyMMddhhmmss");
+console.log(new DateFormat().format("yYyyMMDdhhmmss"));
+console.log(new DateFormat().format("yYyyMMDDHhmmss"));
+console.log(new DateFormat().format("yYyyMMddhhmmss"));
+console.log(new DateFormat().format("yyyyMMddhhmmss"));
