428f06197ffde9092746b3d1036c17dc90155a53
advence-liz <1012515183@qq.com>
2018-02-07 16:42:44 +0800

    update

--- src/index.js
+++ src/index.js
@@ -3,42 +3,47 @@ import DateFormat from "./DateFormat";
 import dateParse from "./DateParse";
 import { convertDate2Ticks, convertTicks2Date, convertDate2Times, convertTimes2Date } from './DateConvert';
 import _ from "lodash";
-const log = console.log;
+const { log, assert, group, groupEnd } = console;
 var dateStr = new DateFormat().format("yYyyMMDdhhmmss");
 var dateStr1 = new DateFormat().format("yYyy-MM-Dd-hh-mm-ss");
-console.group('Date Format');
-console.log('yYyyMMDdhhmmss', new DateFormat().format("yYyyMMDdhhmmss"));
-console.log('yYyyMMDDHhmmss', new DateFormat().format("yYyyMMDDHhmmss"));
-console.log('yYyyMMddhhmmss', new DateFormat().format("yYyyMMddhhmmss"));
-console.log('yyyyMMdDHhmmss', new DateFormat().format("yyyyMMdDHhmmss"));
-console.groupEnd('Date Format');
+group('Date Format');
+log('yYyyMMDdhhmmss', new DateFormat().format("yYyyMMDdhhmmss"));
+log('yYyyMMDDHhmmss', new DateFormat().format("yYyyMMDDHhmmss"));
+log('yYyyMMddhhmmss', new DateFormat().format("yYyyMMddhhmmss"));
+log('yyyyMMdDHhmmss', new DateFormat().format("yyyyMMdDHhmmss"));
+groupEnd('Date Format');
 
-console.group("Date Parse");
-console.log(dateStr);
-console.log(dateParse(dateStr, 'yYyyMMDdhhmmss'));
-console.log(dateStr1);
-console.log(dateParse(dateStr1, 'yYyy-MM-Dd-hh-mm-ss'));
-console.groupEnd("Date Parse");
+group("Date Parse");
+log(dateStr);
+log(dateParse(dateStr, 'yYyyMMDdhhmmss'));
+log(dateStr1);
+log(dateParse(dateStr1, 'yYyy-MM-Dd-hh-mm-ss'));
+groupEnd("Date Parse");
 
-console.group("Date Convert");
+group("convertTimes2Date");
 var now = _.now();
 var dateNow = new Date(now);
-console.assert(now == convertDate2Times(dateNow), 'now!==convertDate2Times(dateNow)');
-console.log(convertDate2Times(dateNow));
-console.log(now);
-console.log(convertTimes2Date(now, 0));
-console.log(convertTimes2Date(now));
-console.log(convertTimes2Date(now, 9));
-console.log(convertTimes2Date(now, 10));
-console.log(dateNow);
+assert(now == convertDate2Times(dateNow), 'now!==convertDate2Times(dateNow)');
+log(convertDate2Times(dateNow));
+log(now);
+log(convertTimes2Date(now, 0));
+log(convertTimes2Date(now));
+log(convertTimes2Date(now, 9));
+log(convertTimes2Date(now, 10));
+log(dateNow);
 
-console.groupEnd("Date Convert");
-log(convertTicks2Date(636480768711190000,8))
- 
- //Tue Dec 05 2017 21:21:11 GMT+0800 (China Standard Time)
- log(convertTicks2Date(636480768711190000,9))
- //Tue Dec 05 2017 22:21:11 GMT+0800 (China Standard Time)
- log(convertTicks2Date(636480768711190000,-5))
- //Tue Dec 05 2017 08:21:11 GMT+0800 (China Standard Time)
- log(convertTicks2Date(636480768711190000,-540))
- //Tue Dec 05 2017 22:21:11 GMT+0800 (China Standard Time)
+groupEnd("convertTimes2Date");
+
+group('Convert ticks to date');
+log(convertTicks2Date(636480768711190000, 8));
+log(convertDate2Ticks(convertTicks2Date('636480768711190000',8)));
+log(636480768711190000);
+//Tue Dec 05 2017 21:21:11 GMT+0800 (China Standard Time)
+log(convertTicks2Date(636480768711190000, 9))
+//Tue Dec 05 2017 22:21:11 GMT+0800 (China Standard Time)
+log(convertTicks2Date(636480768711190000, -5))
+//Tue Dec 05 2017 08:21:11 GMT+0800 (China Standard Time)
+log(convertTicks2Date(636480768711190000, -540))
+//Tue Dec 05 2017 22:21:11 GMT+0800 (China Standard Time)
+
+groupEnd('Convert ticks to date');
